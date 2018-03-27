a34fe8be9b7d88995d1b460228604e63ddfba077
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-03-27 16:30:55 +0200

    Modified factories

--- database/factories/ProfileResourceFactory.php
+++ database/factories/ProfileResourceFactory.php
@@ -5,11 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\ProfileResource::class, function (Faker $faker) {
     return [
         'user_id' => rand(1,5),
-        'qualifier' => $faker->randomElement([
-            "I'm interested in",
-            "I can help with",
-            "I have",
-        ]),
+        'qualifier_id' => rand(1,3),
         'resource_id' => rand(1,5),
     ];
 });
