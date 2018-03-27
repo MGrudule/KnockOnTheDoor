d1efe2380e2c22e97a14c4c62129131404ba8d63
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-03-27 16:44:09 +0200

    Modified DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,16 +13,19 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CircleSeeder::class);
         $this->call(UserSeeder::class);
-        // $this->call(UserCircleSeeder::class);
+        $this->call(UserCircleSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(UserCategorySeeder::class);
 
+        $this->call(ResourceSeeder::class);
         $this->call(UserResourceCategorySeeder::class);
-        $this->call(UserResourceSeeder::class);
+        // $this->call(UserResourceSeeder::class);
+        $this->call(UserResource1Seeder::class);
+        $this->call(UserResource2Seeder::class);
+        $this->call(UserResource3Seeder::class);
 
         // $this->call(ProfileSeeder::class);
-        $this->call(ResourceSeeder::class);
-        $this->call(ProfileResourceQualifierSeeder::class);
-        $this->call(ProfileResourceSeeder::class);
+        // $this->call(ProfileResourceQualifierSeeder::class);
+        // $this->call(ProfileResourceSeeder::class);
     }
 }
