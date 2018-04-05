a8a8c628d69df3f9a5f2da2b194be3b3b1a32724
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-04-05 15:50:06 +0200

    Added public image url and errors

--- app/Http/Resources/UserProfileResource.php
+++ app/Http/Resources/UserProfileResource.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserProfileResource extends JsonResource
 {
+    function __construct($profile, $errors = [])
+    {
+        parent::__construct($profile);
+        $this->errors = $errors;
+    }
+
     /**
      * Transform the resource into an array.
      *
@@ -19,10 +25,23 @@ class UserProfileResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'summary' => $this->summary,
+            'image' => $this->imagePublicUrl(),
             'circle' => new CircleResource($this->circle),
             'categories' => CategoryResource::collection($this->categories->sortBy('name')),
             'resources' => $this->resourcesGroupedByCategory(),
         ];
     }
 
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'errors' => $this->errors,
+        ];
+    }
 }
