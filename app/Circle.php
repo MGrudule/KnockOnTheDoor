981d1871fe0beb09ee4d7cafbcc05e93fdc1e287
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-04-06 14:17:04 +0200

    Added users()

--- app/Circle.php
+++ app/Circle.php
@@ -2,10 +2,16 @@
 
 namespace App;
 
-use App\Circle;
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Circle extends Model
 {
     protected $fillable = ['title', 'description'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
 }
