2b3115d1c3db77dcc3aae5e4a5c8308964d59ba8
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-04-10 09:23:20 +0200

    Added errors

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-6">
+            @include('flash::message')
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
             <div class="card">
                 <img src="/logo.svg" style="width:100%">
             </div>
